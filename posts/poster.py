344cbc65f2cf16fd6398d2ec7c91f2e62623461d
erikmichaelson <47643317+erikmichaelson@users.noreply.github.com>
2021-01-05 21:19:40 -0600

    been blipping on the couch since the 12th grade

--- posts/poster.py
+++ posts/poster.py
@@ -16,13 +16,28 @@ def createPost(postName):
 	final = halves[0] + body + halves[1]
 	return final
 
+def updateLinks(postname):
+	# painfully inefficient. Where's SED -i @?
+	archfd = open('../archive.html', 'r')
+	arch = archfd.read()
+	archfd.close()
+	# bad engineering woooooo
+	arch = arch.split('<!-- BAD -->')
+	newlink = f'<!-- BAD -->\n<a href="posts/{postname}.html"><h3>{postname}</h3></a>'
+	arch = arch[0] + newlink + arch[1]
+	archfd = open('../archive.html', 'w')
+	archfd.write(arch)
+	archfd.close()
+
 
 if __name__ == '__main__':
 	postname = sys.argv[1]
-	print(postname)
 	f = createPost(postname)
-	postname = postname.split('.')
-	print(postname)
-#	newf = open(f'{postname}.html', 'x')
-#	newf.write(f)
-	print(f)
+
+	os.remove(f'{postname}')
+
+	postname = postname.split('.')[0]
+	newf = open(f'{postname}.html', 'x')
+	newf.write(f)
+	updateLinks(postname)
+	print(f'"Successfully" added new post {postname}')
